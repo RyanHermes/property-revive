e32226cce8067153a9d2588fdcfb15eb53aeec58
Abboudy-Ryan Hermes <boudyhermes@gmail.com>
2024-04-12 15:16:19 -0400

    fix: changed image comparison size

--- components/ImageComparison.tsx
+++ components/ImageComparison.tsx
@@ -6,8 +6,8 @@ import Image from 'next/image';
 export default function ImageComparison() {
     return (
         <ImgComparisonSlider>
-            <Image slot='first' src="/before.png" alt="logo" width={1000} height={1000} />
-            <Image slot='second' src="/after.png" alt="logo" width={1000} height={1000} />
+            <Image slot='first' src="/before.png" alt="logo" width={500} height={500} />
+            <Image slot='second' src="/after.png" alt="logo" width={500} height={500} />
         </ImgComparisonSlider>
     );
 }
