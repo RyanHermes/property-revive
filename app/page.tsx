23786ee2736964e1924019f77fdc7e2a93c8c7e0
Abboudy-Ryan Hermes <boudyhermes@gmail.com>
2024-04-12 15:18:23 -0400

    feat: added social media buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,13 @@
 import Image from 'next/image';
 import ImageComparison from '@/components/ImageComparison';
-import SocialMedia from '@/components/SocialMedia';
 
 export default function Home() {
   return (
     <main>
-      <div className="header">
-        <Image src="/pr_name.svg" alt="logo" width={500} height={300} priority />
-        <a href="#contact">Contact</a>
-        <a href="#about">About</a>
-        <a className="active" href="#home">Home</a>
-      </div>
-
       <div className="hero-image">
         <div className="hero-text">
           <h1>Property Revive</h1>
-          <p>Welcome to Property Revive, where we redefine exterior maintenance with comprehensive services that go beyond the ordinary. Specializing in expert window washing and thorough gutter cleaning, we also offer a range of additional exterior solutions to elevate the beauty and functionality of your property. From pressure washing to roof cleaning and beyond, our skilled team is dedicated to restoring and preserving the integrity of your exterior surfaces. With a commitment to excellence and attention to detail, Property Revive ensures that every aspect of your property receives the care it deserves. Trust us to revive your property&apos;s exterior, leaving it sparkling, refreshed, and rejuvenated.</p>
+          <p>Welcome to Property Revive, where we redefine exterior maintenance with comprehensive services that go beyond the ordinary. Specializing in expert window washing and thorough gutter cleaning, we also offer a range of additional exterior solutions to elevate the beauty and functionality of your property. From pressure washing to roof cleaning and beyond, our skilled team is dedicated to restoring and preserving the integrity of your exterior surfaces.<br /><br /> With a commitment to excellence and attention to detail, Property Revive ensures that every aspect of your property receives the care it deserves. Trust us to revive your property&apos;s exterior, leaving it sparkling, refreshed, and rejuvenated.</p>
         </div>
       </div>
 
@@ -32,10 +24,6 @@ export default function Home() {
         <div className="comparison">
           <ImageComparison />
         </div>
-      </div>
-
-      <div className="footer">
-        <p>&copy; 2024 Property Revive</p>
       </div>
     </main>
   );
--- components/SocialMedia.tsx
+++ components/SocialMedia.tsx
@@ -1,10 +1,11 @@
-import { SocialIcon } from 'react-social-icons/component'
+import React from 'react'
+import { SocialIcon } from 'react-social-icons'
 
 export default function SocialMedia() {
     return (
         <div className="social">
-            <SocialIcon url="https://www.facebook.com/people/PropertyRevive/61555246303296/" />
-            <SocialIcon url="https://www.instagram.com/property_revive/" />
+            <SocialIcon url="https://www.facebook.com/people/PropertyRevive/61555246303296/" style={{ margin: "5px" }}/>
+            <SocialIcon url="https://www.instagram.com/property_revive/" style={{ margin: "5px" }}/>
         </div>
     );
 }
