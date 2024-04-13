e6847463efba5967d9de699801401b89a7cfe074
Ryan Hermes <boudyhermes@gmail.com>
2024-04-13 04:36:39 -0400

    fix: favicon.ico

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,10 @@ import Footer from '@/components/Footer'
 export const metadata: Metadata = {
     title: "Property Revive" as string,
     description: "Property Revive is a property management company that helps you manage your property." as string,
+    icons : {
+        icon: "/favicon.ico",
+    }
+
 }
 
 export default function RootLayout({
