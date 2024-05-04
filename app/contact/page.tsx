0dd4b2d80fef58753321ef55d8b1bddd9359bc58
RyanHermes <boudyhermes@gmail.com>
2024-05-04 04:07:58 -0400

    refactor: remove unused code and update header navigation links

--- app/contact/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Contact() {
-    return (
-        <main>
-            <h1>Contacts</h1>
-        </main>
-    )
-}
--- components/Header.tsx
+++ components/Header.tsx
@@ -8,16 +8,15 @@ export default function Header() {
     const pathname = usePathname()
 
     return (
-        <header className="header">
+        <header className="header fixed top-0 left-0 w-full z-50">
             <Link href="/">
                 <Image src="/pr_name.svg" alt="logo" width={300} height={50}  priority />
             </Link>
             <div className="headerLinks">
                 <nav>
-                    <Link className={pathname === "/contact" ? "active" : ""} href="/contact">Contact Us</Link>
-                    <Link className={pathname === "/services" ? "active" : ""} href="/services">Services</Link>
-                    <Link className={pathname === "/about" ? "active" : ""} href="/about">About</Link>
-                    <Link className={pathname === "/" ? "active" : ""} href="/">Home</Link>
+                    <a className={pathname === "#contact" ? "active" : ""} href="#contact">Contact Us</a>
+                    <a className={pathname === "#services" ? "active" : ""} href="#services">Services</a>
+                    <a className={pathname === "#about" ? "active" : ""} href="#about">About</a>
                 </nav>
             </div>
         </header>
