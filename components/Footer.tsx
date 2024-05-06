393bf8e717cf42f0fa0df3a91675733e5b2a0c8d
RyanHermes <boudyhermes@gmail.com>
2024-05-06 19:28:57 -0400

    refactor: optimize footer component and update copyright information

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,37 +4,17 @@ import Link from 'next/link'
 export default function Footer() {
     return (
         <footer className="footer">
-            <div className="footer-content">
-                <div className="footer-section">
-                    <Link href="#" className="footer-link">Locations</Link>
-                    <Link href="#" className="footer-link">FAQ</Link>
-                    <Link href="#" className="footer-link">Blog</Link>
-                </div>
-                <div className="footer-section">
-                    <Link href="#" className="footer-link">Franchising</Link>
-                    <Link href="#" className="footer-link">Careers</Link>
-                    <Link href="#" className="footer-link">Contact Us</Link>
-                </div>
-                <div className="footer-section">
-                    <SocialMedia />
-                </div>
-                <div className="footer-section">
-                    <button className="estimate-button"><strong>Request estimate</strong></button>
-                    <Link href="tel:+10000000000" className="phone-number">(000) 000-0000</Link>
-                </div>
+            <div className="wave">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                    <path fill="#0099ff" fill-opacity="1" d="M0,96L48,106.7C96,117,192,139,288,138.7C384,139,480,117,576,122.7C672,128,768,160,864,176C960,192,1056,192,1152,186.7C1248,181,1344,171,1392,165.3L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+                </svg>
             </div>
-            <div className="footer-bottom">
-                <span>© 2024 All Rights Reserved.</span>
-                <div className="footer-links">
-                    <Link href="#" className="footer-link">Site Map</Link>
-                    <Link href="#" className="footer-link">Accessibility</Link>
-                    <Link href="#" className="footer-link">Privacy Policy</Link>
-                    <Link href="#" className="footer-link">Canada Privacy Policy</Link>
-                    <Link href="#" className="footer-link">Site Search</Link>
-                </div>
-                <div className="footer-statement">
-                    Our services are available to all members of the public regardless of race, gender or sexual
-                    orientation.
+            <div className="footer-content">
+                <div className="footer-content-left">
+                    {/* Copyright */}
+                    <p><strong>© 2021 Property Revive</strong> All Rights Reserved</p>
+                    <p>Anthony Hatoum & Ryan Hermes</p>
+                    <SocialMedia />
                 </div>
             </div>
         </footer>
