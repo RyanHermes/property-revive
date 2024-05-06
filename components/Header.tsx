8b3e42a852c934f5c0da2971906e859e4bcf4915
RyanHermes <boudyhermes@gmail.com>
2024-05-06 19:28:22 -0400

    refactor: optimize code and update header navigation links

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,21 +2,43 @@
 
 import Link from 'next/link'
 import Image from 'next/image'
-import { usePathname } from 'next/navigation'
+import { Link as ScrollLink, Events, scrollSpy } from 'react-scroll'
+import { useEffect } from 'react';
 
 export default function Header() {
-    const pathname = usePathname()
+    useEffect(() => {
+        Events.scrollEvent.register('begin', function () {
+            console.log("begin", arguments);
+        });
+
+        Events.scrollEvent.register('end', function () {
+            console.log("end", arguments);
+        });
+
+        scrollSpy.update();
+
+        return () => {
+            Events.scrollEvent.remove('begin');
+            Events.scrollEvent.remove('end');
+        };
+    }, []);
 
     return (
         <header className="header fixed top-0 left-0 w-full z-50">
             <Link href="/">
-                <Image src="/pr_name.svg" alt="logo" width={300} height={50}  priority />
+                <Image src="/pr_name.svg" alt="logo" width={300} height={50} priority />
             </Link>
             <div className="headerLinks">
                 <nav>
-                    <a className={pathname === "#contact" ? "active" : ""} href="#contact">Contact Us</a>
-                    <a className={pathname === "#services" ? "active" : ""} href="#services">Services</a>
-                    <a className={pathname === "#about" ? "active" : ""} href="#about">About</a>
+                    <ScrollLink activeClass="active" to="contact" spy={true} smooth={true} offset={-70} duration={500}>
+                        Contact Us
+                    </ScrollLink>
+                    <ScrollLink activeClass="active" to="services" spy={true} smooth={true} offset={-70} duration={500}>
+                        Services
+                    </ScrollLink>
+                    <ScrollLink activeClass="active" to="about" spy={true} smooth={true} offset={-70} duration={500}>
+                        About
+                    </ScrollLink>
                 </nav>
             </div>
         </header>
