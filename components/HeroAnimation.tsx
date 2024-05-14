c82cf9b5164ec12b98056d5f4ac23cb30da10444
RyanHermes <boudyhermes@gmail.com>
2024-05-13 20:27:35 -0400

    refactor: optimize positioning of sparkle icons in HeroAnimation component

--- components/HeroAnimation.tsx
+++ components/HeroAnimation.tsx
@@ -13,8 +13,8 @@ export default function HeroAnimation() {
     return (
         <animated.div className="hero-text mt-20 md:mt-0" style={fade}>
             <div className="relative">
-                <Image className="sparkle-big absolute -top-12 -left-12" src="/icon_sparkle.36.ffffff.svg" alt="Sparkle Icon" width={100} height={100} />
-                <Image className="sparkle-small absolute -top-16 left-12" src="/icon_sparkle.36.ffffff.svg" alt="Sparkle Icon" width={50} height={50} />
+                <Image className="sparkle-big absolute ml-5 -top-20 -left-12" src="/icon_sparkle.36.ffffff.svg" alt="Sparkle Icon" width={100} height={100} />
+                <Image className="sparkle-small absolute ml-5 -top-24 left-12" src="/icon_sparkle.36.ffffff.svg" alt="Sparkle Icon" width={50} height={50} />
                 <h1 className='text-center'>Property Revive</h1>
             </div>
             <h2 className="font-normal">Let Property Revive Refresh Your Home!</h2>
